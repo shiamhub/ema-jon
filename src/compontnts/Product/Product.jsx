8f9b927d65163ebcfce411733f9e205d365f2153
shiamhub <skshiam@gmail.com>
2023-03-27 01:14:16 +0600

    final card added

--- src/compontnts/Product/Product.jsx
+++ src/compontnts/Product/Product.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.products)
-    const {img, name, price, seller, quantity} = props.products
+    // console.log(props.products)
+    const {img, name, price, rating, seller, quantity} = props.products
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h4>{name}</h4>
+            <div className='p-disc'>
+                <h6>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {rating} Stars</p>
+
+            </div>
+            <button className='btn-card'>Add to Card</button>
         </div>
     );
 };
