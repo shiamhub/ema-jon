0049b2ca1fafcddf2b67ff6cca4944a553a9f105
shiamhub <skshiam@gmail.com>
2023-03-28 00:08:28 +0600

    save card localstoreg

--- src/compontnts/Shop/Shop.jsx
+++ src/compontnts/Shop/Shop.jsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Card from '../Card/Card';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [card, setCard] = useState([])
+    const [card, setCard] = useState([]);
     // console.log(card);
     
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[])
+    },[]);
+
+    useEffect(() => {
+        const newCard = getShoppingCart();
+        console.log(newCard);
+    }, [])
     
     const handleAddCard = (product) => {
         const newCard = [...card, product];
         setCard(newCard);
+        addToDb(product.id)
     }
 
     return (
